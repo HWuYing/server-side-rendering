6a4f21ce2abd18638008ada7829364f973a10ac6
hwyn <237617561@qq.com>
2023-03-18 11:46:17 +0800

    update

--- micro/micro-manage/micro-manage.d.ts
+++ micro/micro-manage/micro-manage.d.ts
@@ -1,13 +1,12 @@
 import { Injector } from '@fm/di';
 import { MicroManageInterface } from '@fm/shared/micro';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 export declare class MicroManage implements MicroManageInterface {
     private injector;
     sharedData: any;
     private resource;
     private microStaticCache;
     private appContext;
-    loaderStyleSubject?: Subject<HTMLStyleElement> | undefined;
     constructor(injector: Injector);
     bootstrapMicro(microName: string): Observable<any>;
     private checkRedirect;
